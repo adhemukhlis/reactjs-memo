06c40b2b6f2e3399eb5edeee8cbe43d9c8610745
adhemukhlis <adhemukhlis@gmail.com>
2022-04-23 00:12:34 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import React from 'react'
-import './style.css'
-const Component1 = React.memo(function Component1(props) {
-  console.log("Component 1 rendered")
+import React from 'react';
+import './style.css';
+const Component1 = React.memo((props) => {
+  console.log('Component 1 rendered');
   return (
     <div>
-      <p>Component 1</p>
-      </div>
-  )
+      <p>Component 1 count: {props.count}</p>
+    </div>
+  );
 });
 
 const Component2 = () => {
-  console.log("Component 2 rendered")
+  console.log('Component 2 rendered');
   return (
     <div>
       <p>Component 2</p>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
 const App = () => {
-  const [counter, setCounter] = React.useState(0)
+  const [counter, setCounter] = React.useState(0);
+  const [counter2, setCounter2] = React.useState(0);
   return (
     <div>
       <div>Count: {counter}</div>
-      <Component1 />
+      <div>Count 2: {counter2}</div>
+      <Component1 count={counter} />
       <Component2 />
       <button onClick={() => setCounter(counter + 1)}>increase count</button>
-  </div>
-  )
-}
-export default App
+      <button onClick={() => setCounter2(counter + 1)}>increase count 2</button>
+    </div>
+  );
+};
+export default App;
