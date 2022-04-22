c27778ef93426acc866f1fa16f002158bf2a499d
adhemukhlis <adhemukhlis@gmail.com>
2022-04-23 02:06:07 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import React from 'react';
 import './style.css';
-const Component1 = React.memo((props) => {
-  console.log('Component 1 rendered');
+const ComponentA = React.memo((props) => {
+  console.log('Component A rendered');
   return (
     <div>
-      <p>Component 1 count: {props.count}</p>
+      <p>Component A count: {props.count}</p>
     </div>
   );
 });
 
-const Component2 = () => {
-  console.log('Component 2 rendered');
+const ComponentB = (props) => {
+  console.log('Component B rendered');
   return (
     <div>
-      <p>Component 2</p>
+      <p>Component B count: {props.count}</p>
     </div>
   );
 };
 
 const App = () => {
-  const [counter, setCounter] = React.useState(0);
-  const [counter2, setCounter2] = React.useState(0);
+  const [counterA, setCounterA] = React.useState(0);
+  const [counterB, setCounterB] = React.useState(0);
   return (
     <div>
-      <div>Count: {counter}</div>
-      <div>Count 2: {counter2}</div>
-      <Component1 count={counter} />
-      <Component2 />
-      <button onClick={() => setCounter(counter + 1)}>increase count</button>
-      <button onClick={() => setCounter2(counter + 1)}>increase count 2</button>
+      <div>Count A: {counterA}</div>
+      <div>Count B: {counterB}</div>
+      {/* saat increase count B, memo akan tetap re-render karena komponen mount ulang */}
+      {/* {
+        counterB % 2 === 0 && <ComponentA count={counterA} />
+      } */}
+
+      {/* gunakan style display:none pada parent untuk hide komponen memo */}
+      <div style={{ display: counterB % 2 === 1 ? 'none' : '' }}>
+        <ComponentA count={counterA} />
+      </div>
+
+      <ComponentB count={counterB} />
+      <button onClick={() => setCounterA((prev) => (prev += 1))}>
+        increase count A
+      </button>
+      <button onClick={() => setCounterB((prev) => (prev += 1))}>
+        increase count B
+      </button>
     </div>
   );
 };
